4c389781ab49b37c49480cadf48b089045c622cc
LunarSalameh <lun20190983@std.psut.edu.jo>
2023-09-25 15:38:23 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number) {
   // Write your code here!
+  if (number <= 400)
+    return "This one is on me!";
+  else if (number > 400 && number <= 2000)
+    return "That will be twenty bucks.";
+  else if (number > 2000 && number < 2500)
+    return "I will gladly take your thirty bucks.";
+  else if (number > 2500)
+    return "No can do.";
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName) {
   // Write your code here!
+  switch (cityName) {
+    case 'NYC': return "Ok, sounds good.";
+    default: return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+
+  switch (tip) {
+    case 'generous': return "Thank you so much.";
+    case 'not as generous': return "Thank you.";
+    case 'thanks for everything': return "Bye.";
+  }
 }
